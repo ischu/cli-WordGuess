05b9e13b0ee4728d7db80291202486b9374aa82f
ianschu <ianschu7@gmail.com>
2019-02-02 12:43:44 -0500

    added inquirer prompt

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
 //   * Randomly selects a word and uses the `Word` constructor to store it
 //   * Prompts the user for each guess and keeps track of the user's remaining guesses
-// 3. `Letter.js` *should not* `require` any other files.
-// 4. `Word.js` *should only* require `Letter.js`
+
+const Word = require("./Word");
+const inquirer = require("inquirer");
+let secretWord = "happy";
+let guessCount = 10;
+
+// inquirer prompt for entering letter
+function inquire(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "guess",
+            message: "GUESS A LETTER!",
+            // makes sure input is single letter
+            validate: function (value) {
+                var pass = value.match(/[a-zA-z]/) && value.length === 1;
+                if (pass) {
+                    return true
+                } else {
+                    return "please enter a letter";
+                }
+            }
+        }
+    ]).then(function (user) {
+        {
+            let guessLetter = user.guess;
+            return guessLetter;
+        }
+    });
+};
+
+function guess() {
+
+}
